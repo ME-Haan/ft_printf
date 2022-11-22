e288c888a5c63c438c19e0cf8a1d32db5275b20c
Martin Haan <mhaan@f1r4s8.codam.nl>
2022-11-22 14:40:06 +0100

    Fixed paco strict

--- src/switch.c
+++ src/switch.c
@@ -6,7 +6,7 @@
 /*   By: mhaan <mhaan@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/29 17:25:59 by mhaan         #+#    #+#                 */
-/*   Updated: 2022/11/22 13:22:27 by mhaan         ########   odam.nl         */
+/*   Updated: 2022/11/22 14:38:35 by mhaan         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	fmt_switch(char fmt, va_list arg, int *len)
 	else if (fmt == 'u')
 		*len += put_uns_base(va_arg(arg, unsigned int), 10, fmt);
 	else if (fmt == 'p')
-		*len += put_uns_base(va_arg(arg, size_t), 16, fmt);
+		*len += put_uns_base(va_arg(arg, intptr_t), 16, fmt);
 	else if (fmt == 'x' || fmt == 'X')
 		*len += put_uns_base(va_arg(arg, unsigned int), 16, fmt);
 	else if (fmt == '%')
